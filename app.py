0657fb9d9620863cc9e3836b3c9ae1d359ac86bb
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2021-06-02 20:20:11 +0500

    Init bot messenger template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('bot_page.html')
 
 
 if __name__ == '__main__':
